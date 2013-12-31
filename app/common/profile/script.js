8e4e1138faecbf671c98526c17dcffa6914c65cf
surya.dakshina <surya.dakshina@gmail.com>
2014-01-01 03:06:32 +0530

    modify UI + more jQuery

--- app/common/profile/script.js
+++ app/common/profile/script.js
@@ -13,4 +13,11 @@ $(document).ready(function(){
     $("#flwing").mouseleave(function(){
        $(this).fadeTo('fast',0.75); 
     });
+    $("#header").click(function(){
+        $(this).css('background-color','#9B59B6');    
+    });
+    $("#usrattributes").click(function(){
+        $(this).animate({left:"+=25px"},500);    
+    });
+    $("#usrattributes").accordion();
 });
